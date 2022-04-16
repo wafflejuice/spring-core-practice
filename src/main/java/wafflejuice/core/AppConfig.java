81742486816a771389684992c338c1f77c87959b
wafflejuice <wafflejuice96@gmail.com>
2022-04-16 15:22:37 +0900

    refactoring

--- src/main/java/wafflejuice/core/AppConfig.java
+++ src/main/java/wafflejuice/core/AppConfig.java
@@ -1,6 +1,8 @@
 package wafflejuice.core;
 
+import wafflejuice.core.discount.DiscountPolicy;
 import wafflejuice.core.discount.FixDiscountPolicy;
+import wafflejuice.core.member.MemberRepository;
 import wafflejuice.core.member.MemberService;
 import wafflejuice.core.member.MemberServiceImpl;
 import wafflejuice.core.member.MemoryMemberRepository;
@@ -8,11 +10,20 @@ import wafflejuice.core.order.OrderService;
 import wafflejuice.core.order.OrderServiceImpl;
 
 public class AppConfig {
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
