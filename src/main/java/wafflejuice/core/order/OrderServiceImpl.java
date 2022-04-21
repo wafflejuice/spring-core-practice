43ab180acf1ffcb84129e1da2ec0c9855cba797d
wafflejuice <wafflejuice96@gmail.com>
2022-04-21 21:34:31 +0900

    lombok library

--- src/main/java/wafflejuice/core/order/OrderServiceImpl.java
+++ src/main/java/wafflejuice/core/order/OrderServiceImpl.java
@@ -1,22 +1,17 @@
 package wafflejuice.core.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import wafflejuice.core.discount.DiscountPolicy;
 import wafflejuice.core.member.Member;
 import wafflejuice.core.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
